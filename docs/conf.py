9e78994d7167cbdf5fb7e6996409bba6d7c6a0e9
prolic <saschaprolic@googlemail.com>
2016-06-18 17:22:57 +0800

    update conf.py for docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# HumusAmqpModule documentation build configuration file, created by
+# HumusAmqp documentation build configuration file, created by
 # sphinx-quickstart on Wed Aug 27 13:21:24 2014.
 #
 # This file is execfile()d with the current directory set to its
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Humus AMQP Module'
-copyright = u'2014, Sascha-Oliver Prolic'
+project = u'HumusAmqp'
+copyright = u'2016, Sascha-Oliver Prolic'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -176,7 +176,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'HumusAmqpModuledoc'
+htmlhelp_basename = 'HumusAmqpdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -196,7 +196,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'HumusAmqpModule.tex', u'HumusAmqpModule Documentation',
+  ('index', 'HumusAmqp.tex', u'HumusAmqp Documentation',
    u'Sascha-Oliver Prolic', 'manual'),
 ]
 
@@ -226,7 +226,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'humusamqpmodule', u'HumusAmqpModule Documentation',
+    ('index', 'humusamqp', u'HumusAmqp Documentation',
      [u'Sascha-Oliver Prolic'], 1)
 ]
 
@@ -240,8 +240,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'HumusAmqpModule', u'HumusAmqpModule Documentation',
-   u'Sascha-Oliver Prolic', 'HumusAmqpModule', 'One line description of project.',
+  ('index', 'HumusAmqp', u'HumusAmqp Documentation',
+   u'Sascha-Oliver Prolic', 'HumusAmqp', 'One line description of project.',
    'Miscellaneous'),
 ]
 
@@ -261,13 +261,13 @@ texinfo_documents = [
 # -- Options for Epub output ----------------------------------------------
 
 # Bibliographic Dublin Core info.
-epub_title = u'HumusAmqpModule'
+epub_title = u'HumusAmqp'
 epub_author = u'Sascha-Oliver Prolic'
 epub_publisher = u'Sascha-Oliver Prolic'
-epub_copyright = u'2014, Sascha-Oliver Prolic'
+epub_copyright = u'2016, Sascha-Oliver Prolic'
 
 # The basename for the epub file. It defaults to the project name.
-#epub_basename = u'HumusAmqpModule'
+#epub_basename = u'HumusAmqp'
 
 # The HTML theme for the epub output. Since the default themes are not optimized
 # for small screen space, using the same theme for HTML and epub output is
